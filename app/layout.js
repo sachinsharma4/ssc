6d8cf6bd34b7276ac44945ebc813636d63926da2
sachinsharma4 <sachinsharma7673@gmail.com>
2025-03-26 20:30:30 +0530

    added a contact ui

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden  `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden bg-blue-300 `}
       >
         {children}
       </body>
--- app/page.js
+++ app/page.js
@@ -2,10 +2,12 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import Image from "next/image";
 import Link from 'next/link';
 import React from 'react';
+import { AiOutlineDown } from "react-icons/ai";
+import { Field, Label, Switch } from '@headlessui/react'
 
 const FacultyProfileCard = () => {
   return (
-    <div className="border-1 m-2 lg:m-1.5 min-w-72  p-6 rounded-lg shadow-lg  flex flex-col items-start relative">
+    <div className="border-1 m-2 lg:m-1.5 min-w-72 bg-white p-6 rounded-lg shadow-lg  flex flex-col items-start relative">
       <Image
         src="/image.png"
         alt="Profile"
@@ -24,6 +26,153 @@ const FacultyProfileCard = () => {
     </div>
   );
 };
+
+
+
+
+
+const  Example = () => {
+ 
+
+  return (
+    <div className="isolate  px-6 py-24 sm:py-32 lg:px-8">
+      <div
+        aria-hidden="true"
+        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+      >
+        <div
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+          className="relative left-1/2 -z-10 aspect-1155/678 w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+        />
+      </div>
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Contact sales</h2>
+        <p className="mt-2 text-lg/8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
+      </div>
+      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div>
+            <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">
+              First name*
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="first-name"
+                name="first-name"
+                type="text"
+                required
+                autoComplete="given-name"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor="last-name" className="block text-sm/6 font-semibold text-gray-900">
+              Last name
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="last-name"
+                name="last-name"
+                type="text"
+                autoComplete="family-name"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              />
+            </div>
+          </div>
+          
+          <div className="sm:col-span-2">
+            <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">
+              Email*
+            </label>
+            <div className="mt-2.5">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autoComplete="email"
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="phone-number" className="block text-sm/6 font-semibold text-gray-900">
+              Phone number*
+            </label>
+            <div className="mt-2.5">
+              <div className="flex rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+                <div className="grid shrink-0 grid-cols-1 focus-within:relative">
+                  <select
+                    id="country"
+                    name="country"
+                    required
+                    autoComplete="country"
+                    aria-label="Country"
+                    className="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pr-7 pl-3.5 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  >
+                    <option>IN</option>
+                    <option>US</option>
+                    <option>EU</option>
+                  </select>
+                  <AiOutlineDown
+                    aria-hidden="true"
+                    className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                  />
+                </div>
+                <input
+                  id="phone-number"
+                  name="phone-number"
+                  type="text"
+                  placeholder="123-456-7890"
+                  className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">
+              Message
+            </label>
+            <div className="mt-2.5">
+              <textarea
+                id="message"
+                name="message"
+                rows={4}
+                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                defaultValue={''}
+              />
+            </div>
+          </div>
+          <Field className="flex gap-x-4 sm:col-span-2">
+            <div className="flex h-6 items-center">
+            
+            </div>
+            <Label className="text-sm/6 text-gray-600">
+              by filling with this you agree with our 
+              <a href="#" className="font-semibold text-indigo-600">
+                privacy&nbsp;policy
+              </a>
+              .
+            </Label>
+          </Field>
+        </div>
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+        submit
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
 const TestimonialCard = () => {
   return (
 
@@ -59,50 +208,64 @@ const TestimonialCard = () => {
 export default function Home() {
   return (
     <div>
+<div>
+<div className="bg-blue-400 w-44  hidden min-h-screen z-20 ">
+  <button className=" block ml-36 text-white pt-2 text-2xl">X</button>
+  <Link href="/about" className="text-white ml-2 hover:text-yellow-300">About
+  </Link>
+  <hr className="mb-2 w-full " />
+  <Link href="/courses" className="text-white  ml-2 hover:text-yellow-300">Courses
+  </Link>
+  <hr  className="mb-2 w-full "/>
+
+  <Link href="/contact" className="text-white ml-2  hover:text-yellow-300">Contact
+  </Link>
+  <hr  className="mb-2 w-full "/>
+
+</div>
+</div>
+<div className="main-ui">
+<nav className="sticky bg-blue-800 z-10 top-0 p-4 shadow-lg  container mx-auto flex justify-between items-center">
+
+<div className="text-white font-bold text-2xl">
+
+  <GiHamburgerMenu className=" inline -mt-1" />
+  <span> SSC Classes</span>
+
+
+</div>
+
+{/* Nav Links */}
+<div className="hidden md:flex space-x-6">
+  <Link href="/about" className="text-white hover:text-yellow-300">About
+  </Link>
+  <Link href="/courses" className="text-white hover:text-yellow-300">Courses
+  </Link>
+  <Link href="/contact" className="text-white hover:text-yellow-300">Contact
+  </Link>
+
+</div>
 
 
 
-
-      <nav className="sticky bg-blue-800 z-10 top-0 p-4 shadow-lg  container mx-auto flex justify-between items-center">
-
-        <div className="text-white font-bold text-2xl">
-
-          <GiHamburgerMenu className=" inline -mt-1" />
-          <span> SSC Classes</span>
+</nav>
 
 
-        </div>
+<div className="p-3.5 container min-w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3  ">
 
-        {/* Nav Links */}
-        <div className="hidden md:flex space-x-6">
-          <Link href="/about" className="text-white hover:text-yellow-300">About
-          </Link>
-          <Link href="/courses" className="text-white hover:text-yellow-300">Courses
-          </Link>
-          <Link href="/contact" className="text-white hover:text-yellow-300">Contact
-          </Link>
-
-        </div>
+<FacultyProfileCard />
+<FacultyProfileCard />
+<FacultyProfileCard />
+<FacultyProfileCard />
+<FacultyProfileCard />
 
 
-
-      </nav>
-
-
-      <div className="p-3.5 container min-w-full flex lg:justify-center overflow-y-hidden  flex-row">
-
-        <FacultyProfileCard />
-        <FacultyProfileCard />
-        <FacultyProfileCard />
-        <FacultyProfileCard />
-        <FacultyProfileCard />
-
-
-      </div> 
-
-      <div className="bg-[url(/image.svg)]  bg-cover w-full">
-      <div className="text-center p-5 font-extrabold text-5xl italic text-blue-400 underline ">Our student Reviews</div>
-        <div className="p-3.5 container  min-w-full flex lg:justify-center overflow-y-hidden flex-row">
+</div> 
+<hr />
+<Example />
+<div className="bg-[url(/image.svg)]  bg-cover w-full">
+<div className="text-center p-5 font-extrabold text-5xl italic text-black underline  ">Our student Reviews</div>
+<div className="p-3.5 container  min-w-full flex lg:justify-center overflow-y-hidden no-scrollbar flex-row">
 
 <TestimonialCard />
 <TestimonialCard />
@@ -113,25 +276,28 @@ export default function Home() {
 
 
 </div>
-    </div>
+</div>
+
+
+
+
+<footer className="sticky w-full mt-3.5 text-green-400  bottom-0   bg-blue-700">
+
+<div className="text-center">
+  ssc classes registerd trademark
+  <ul className="flex flex-row w-3/5 space-x-2.5">
+
+    <li>yoiutube</li>
+    <li>twiter</li>
+    <li>intsa</li>
+    <li>linkdin</li>
+  </ul>
+</div>
+</footer>
+</div>
 
 
     
-    
-     
-      <footer className="sticky w-full mt-3.5 text-green-400  bottom-0   bg-blue-700">
-
-        <div className="text-center">
-          ssc classes registerd trademark
-          <ul className="flex flex-row w-3/5 space-x-2.5">
-
-            <li>yoiutube</li>
-            <li>twiter</li>
-            <li>intsa</li>
-            <li>linkdin</li>
-          </ul>
-        </div>
-      </footer>
     </div>
 
   );
